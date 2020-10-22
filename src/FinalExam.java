20832907b6b6dc5796c5c3213474bd5977c71d11
Ike Maldonado <31159328+ike5@users.noreply.github.com>
2020-10-21 21:40:51 -0700

    Update FinalExam.java
    Changed via github

--- src/FinalExam.java
+++ src/FinalExam.java
@@ -1,29 +1,38 @@
-public class FinalExam extends GradedActivity {
-    private int numQuestions;
-    private double pointsEach;
-    private int numMissed;
-
-    /**
-     * @param questions
-     * @param missed
-     */
-    public FinalExam(int questions, int missed) {
-        double numericScore;
-
-        numQuestions = questions;
-        numMissed = missed;
-
-        pointsEach = 100.0 / questions;
-        numericScore = 100.0 - (missed * pointsEach);
-
-        setScore(numericScore);
-    }
-
-    public double getPointsEach() {
-        return pointsEach;
-    }
-
-    public int getNumMissed() {
-        return numMissed;
+import javax.swing.JOptionPane;
+ 
+/**
+This program demonstrates the FinalExam class,
+which extends the GradedActivity class.
+*/
+ 
+public class FinalExamDemo
+{
+    public static void main(String[] args)
+{
+        String input; // To hold input
+        int questions; // Number of questions
+        int missed; // Number of questions missed
+ 
+        // Get the number of questions on the exam.
+        input = JOptionPane.showInputDialog("How many " +
+            "questions are on the final exam?");
+        questions = Integer.parseInt(input);
+ 
+        // Get the number of questions the student missed.
+        input = JOptionPane.showInputDialog("How many " +
+            "questions did the student miss?");
+        missed = Integer.parseInt(input);
+ 
+        // Create a FinalExam object.
+        FinalExam exam = new FinalExam(questions, missed);
+ 
+        // Display the test results.
+        JOptionPane.showMessageDialog(null,
+            "Each question counts " + exam.getPointsEach() +
+            " points.\nThe exam score is " +
+            exam.getScore() + "\nThe exam grade is " +
+            exam.getGrade());
+ 
+        System.exit(0);
     }
 }
